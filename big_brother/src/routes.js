30a5fa4c9a202bcc8091ca8a2e60e650ff6fe74f
trihazard <nsrirama@smu.edu>
2019-11-07 12:56:56 -0600

    added new pages to routes

--- big_brother/src/routes.js
+++ big_brother/src/routes.js
@@ -3,8 +3,13 @@ import { register } from "./serviceWorker";
 // export default ROUTES;
 import { Register } from './app/authentication/Register';
 import { ParentDashboard } from './app/dashboard/Parent';
+import { DriverDashboard } from './app/dashboard/Driver';
+import { DriverProfile } from './app/profiles/DriverProfile';
+
 export const ROUTES = [
-    { path: '/register', component: Register, authRequired: false, adminRequired: false},
-    { path: '/parent', component: ParentDashboard, authRequired: false, adminRequired: false}
+    { path: '/register', component: Register, authRequired: false, adminRequired: false },
+    { path: '/parent', component: ParentDashboard, authRequired: false, adminRequired: false },
+    { path: '/driver', component: DriverDashboard, authRequired: false, adminRequired: false },
+    { path: '/driver/profile', component: DriverProfile, authRequired: false, adminRequired: false }
 ];
 export default ROUTES;
