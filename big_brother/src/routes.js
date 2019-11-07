71c778847b45a459b0750fdda968ef16f9dff9ed
trihazard <nsrirama@smu.edu>
2019-11-07 17:41:37 -0600

    Drew made pages for parent profiles and code to handle children

--- big_brother/src/routes.js
+++ big_brother/src/routes.js
@@ -9,7 +9,7 @@ import { DriverProfile } from './app/profiles/DriverProfile';
 export const ROUTES = [
     { path: '/register', component: Register, authRequired: false, adminRequired: false },
     { path: '/parent', component: ParentDashboard, authRequired: false, adminRequired: false },
-    { path: '/driver', component: DriverDashboard, authRequired: false, adminRequired: false },
-    { path: '/driver/profile', component: DriverProfile, authRequired: false, adminRequired: false }
+    { path: '/driver/profile', component: DriverProfile, authRequired: false, adminRequired: false },
+    { path: '/driver', component: DriverDashboard, authRequired: false, adminRequired: false }
 ];
 export default ROUTES;
