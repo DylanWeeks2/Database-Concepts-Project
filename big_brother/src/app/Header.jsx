85491289accb167a551ea9b89c11eaa932873d3c
trihazard <nsrirama@smu.edu>
2019-11-17 11:51:24 -0600

    kinda looks nicer

--- big_brother/src/app/Header.jsx
+++ big_brother/src/app/Header.jsx
@@ -12,56 +12,41 @@ export class Header extends React.Component {
 		return (
 			<>
 			<head>
-              <title>Big Brother</title>
-  <meta charset="utf-8"></meta>
-  <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"></link>
-  <link rel="stylesheet" href="sign_up.css"></link>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-            <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-              <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
-        
-                <a class="navbar-brand" href="#">Big Brother</a>
-        
+        <title>Big Brother</title>
+        <link rel="stylesheet" href="sign_up.css"></link>
+      </head>
+      <body>
+          <nav className="navbar bg-dark navbar-inverse">
+            <div className="container-fluid">
+              <div className="navbar-header">   
+                <a className="navbar-brand" href="#">Big Brother</a>
               </div>
-              <div class="collapse navbar-collapse" id="myNavbar">
-        
-                <ul class="nav navbar-nav navbar-right">
-        
-        
-                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        
-                    <form id="signin" class="navbar-form navbar-right" role="form">
-        
-                      <div class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="email" type="email" onchange={this.handleChange} class="form-control" name="email" placeholder="Email Address"/>
+              <div className="navbar-form navbar-right">
+                <form className="form-inline navbar-form navbar-right">
+                  <ul className="list-group-flush">
+                    <li className="list-group-item list-group-item-primary">
+                      <div className="input-group">
+                        <span className="input-group-text">@</span>
+                        <input type="text" className="form-control" placeholder="Username"/>
                       </div>
-        
-                      <div class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="password" type="password" onchange={this.handleChange} class="form-control" name="password"  placeholder="Password"/>
+                    </li>
+                    <li className="list-group-item list list-group-item-primary">
+                      <div className="input-group">
+                        <span className="input-group-text">P</span>
+                        <input type="password" className="form-control" placeholder="Password"/>
                       </div>
-        
-                      <button type="button" class="btn btn-primary" onClick={() => this.Login()}>Login</button>
-                      <button type="button" class="btn btn-primary">Forgot Password</button>
-                    </form>
-        
+                    </li>
+                  </ul>
+                  <div className="btn-group-vertical ml-2">
+                    <button className="btn btn-outline-success mr-3" type="submit">Login</button>
+                    <button className="btn btn-outline-success" type="submit">Forgot Password</button>                    
                   </div>
-                </ul>
+                </form>
               </div>
             </div>
           </nav>
-			  </body>
-				</>
+			</body>
+			</>
 			);
 		}
 }
