f564ed1d85f6f951508f1c5148c22595fc0762b1
trihazard <nsrirama@smu.edu>
2019-11-18 21:45:15 -0600

    created component

---
+++ big_brother/src/app/ParentPastRides.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ParentUser } from '../models/ParentUser';
+import { RideItem } from './dashboard/RideItem';
+import { DriverUser } from '../models/DriverUser';
+
+export class ParentPastRides {
+    state = {
+        rides: [],
+        drivers: []
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
