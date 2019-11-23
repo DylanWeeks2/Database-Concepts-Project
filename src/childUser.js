474317ca49281dde09967c79d555fca6a80163e3
Dylan Weeks <dylanweeks@Dylans-MacBook-Pro.local>
2019-11-23 16:31:45 -0600

    fixed get and update

--- src/childUser.js
+++ src/childUser.js
@@ -30,7 +30,7 @@ query = "alter table childUser autoIncrement = 100000"
 //post /addChild
 exports.addChild = (req, res) => {
   console.log(req.body);
-  let query = "INSERT into childUser values(NULL,'" + req.body.name + "','" + req.body.bio + "','" + req.body.healthConditions + "','" + req.body.emergencyContactName + "','" + req.body.emergencyContactNumber + "','" + req.body.rating + "','" + req.body.parentId + "')"; 
+  let query = "INSERT into childUser values(NULL,'" + req.body.name + "','" + req.body.bio + "','" + req.body.healthConditions + "','" + req.body.emergencyContactName + "','" + req.body.emergencyContactNumber + "','" + req.body.rating + "','" + req.body.parentID + "')"; 
   db.query(query, (err, result) => {
       if(err) {
           logger.error("failed too add child");
@@ -42,12 +42,12 @@ exports.addChild = (req, res) => {
   })
 };
 
-//GET /viewChildHealthConditions
-exports.getChildName = (req, res) => {
+//GET /getChild
+exports.getChild = (req, res) => {
   let query = "select * from childUser where id = '" + req.body.id + "'";
   db.query(query, function(err,rows, fields) {
     if(err){
-        logger.error("couldn't get child name");
+        logger.error("couldn't get child info");
         res.status(400).json({
           "data": [],
           "error": "MySQL error"
@@ -55,110 +55,16 @@ exports.getChildName = (req, res) => {
       }
       else{
         res.status(200).json({
-          "name": rows[0].name
+          "data": rows
         });
       }
   })
 }
 
-//GET /viewChildHealthConditions
-exports.getChildRating = (req, res) => {
-  let query = "select * from childUser where id = '" + req.body.id + "'";
-  db.query(query, function(err,rows, fields) {
-    if(err){
-        logger.error("couldn't get child rating");
-        res.status(400).json({
-          "data": [],
-          "error": "MySQL error"
-        });
-      }
-      else{
-        res.status(200).json({
-          "rating": rows[0].rating
-        });
-      }
-  })
-}
-
-//GET /viewChildHealthConditions
-exports.getChildHealthConditions = (req, res) => {
-  let query = "select * from childUser where id = '" + req.body.id + "'";
-  db.query(query, function(err,rows, fields) {
-    if(err){
-        logger.error("couldn't get healthConditions");
-        res.status(400).json({
-          "data": [],
-          "error": "MySQL error"
-        });
-      }
-      else{
-        res.status(200).json({
-          "healthConditions": rows[0].healthConditions
-        });
-      }
-  })
-}
-
-//GET /viewChildEmergencyContact
-exports.getChildEmergencyContactName = (req, res) => {
-  let query = "select * from childUser where id = '" + req.body.id + "'";
-  db.query(query, function(err,rows, fields) {
-    if(err){
-        logger.error("couldn't get emergency contact name");
-        res.status(400).json({
-          "data": [],
-          "error": "MySQL error"
-        });
-      }
-      else{
-        res.status(200).json({
-          "emergencyContact": rows[0].emergencyContactName
-        });
-      }  
-    })
-}
-
-exports.getChildEmergencyContactNumber = (req, res) => {
-    let query = "select * from childUser where id = '" + req.body.id + "'";
-    db.query(query, function(err,rows, fields) {
-      if(err){
-          logger.error("couldn't get emergency contact number");
-          res.status(400).json({
-            "data": [],
-            "error": "MySQL error"
-          });
-        }
-        else{
-          res.status(200).json({
-            "emergencyContact": rows[0].emergencyContactNumber
-          });
-        }  
-      })
-  }
-
-//GET /viewChildBio
-exports.getChildBio = (req, res) => {
-  let query = "select * from childUser where id = '" + req.body.id + "'";
-  db.query(query, function(err,rows, fields) {
-    if(err){
-        logger.error("couldn't get bio");
-        res.status(400).json({
-          "data": [],
-          "error": "MySQL error"
-        });
-      }
-      else{
-        res.status(200).json({
-          "bio": rows[0].bio
-        });
-      }  
-    })
-}
-
-//post /updateChildName
-exports.updateChildName = (req, res) => {
+//post /updateChild
+exports.updateChild = (req, res) => {
   console.log(req.body);
-  let query = "update child set name = '" + req.body.name + "' where id = '" + req.body.id + "'";
+  let query = "update childUser set name = '" + req.body.name + "', bio = '" + req.body.bio + "', healthConditions = '" + req.body.healthConditions + "', emergencyContactName = '" + req.body.emergencyContactName + "', emergencyContactNumber = '" + req.body.emergencyContactNumber + "', rating = '" + req.body.rating + "' where id = '" + req.body.id + "'";
   db.query(query, (err, result) => {
       if(err) {
           logger.error("failed too update child");
@@ -169,101 +75,3 @@ exports.updateChildName = (req, res) => {
       }
   })
 };
-
-//post /updateChildBio
-exports.updateChildBio = (req, res) => {
-  console.log(req.body);
-  let query = "update car set bio = '" + req.body.bio + "' where id = '" + req.body.id + "'";
-  db.query(query, (err, result) => {
-      if(err) {
-          logger.error("failed too update child");
-          res.status(400);
-      }
-      else{
-          res.status(200).send('child has been updated!!');
-      }
-  })
-};
-  
-
-//post /updateChildHealthConditions
-exports.updateChildHealthConditions = (req, res) => {
-  console.log(req.body);
-  let query = "update child set name = '" + req.body.healthConditions + "' where id = '" + req.body.id + "'";
-  db.query(query, (err, result) => {
-      if(err) {
-          logger.error("failed too update child");
-          res.status(400);
-      }
-      else{
-          res.status(200).send('Child has been updated!!');
-      }
-  })
-};
-
-//post /updateChildEmergencyContactName
-exports.updateChildEmergencyContactName = (req, res) => {
-  console.log(req.body);
-  let query = "update car set emergencyContactName = '" + req.body.emergencyContactName + "' where id = '" + req.body.id + "'";
-  db.query(query, (err, result) => {
-      if(err) {
-          logger.error("failed too update child");
-          res.status(400);
-      }
-      else{
-          res.status(200).send('child has been updated!!');
-      }
-  })
-};
-
-//post /updateChildRating
-exports.updateChildRating = (req, res) => {
-  console.log(req.body);
-  let query = "update child set rating = '" + req.body.rating + "' where id = '" + req.body.id + "'";
-  db.query(query, (err, result) => {
-      if(err) {
-          logger.error("failed too update child");
-          res.status(400);
-      }
-      else{
-          res.status(200).send('Child has been updated!!');
-      }
-  })
-};
-
-//post /updateChildEmergencyContactNumber
-exports.updateChildEmergencyContactNumber = (req, res) => {
-  console.log(req.body);
-  let query = "update car set emergencyContactNumber = '" + req.body.emergencyContactNumber + "' where id = '" + req.body.id + "'";
-  db.query(query, (err, result) => {
-      if(err) {
-          logger.error("failed too update child");
-          res.status(400);
-      }
-      else{
-          res.status(200).send('child has been updated!!');
-      }
-  })
-};
-
-exports.login = (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let query = "select * from parentUser where name = '" + req.body.username + "' and password = '" + req.body.password + "' limit 1";
-  db.query(query, function(err, rows, field) {
-    if(rows == null  ||rows.length == 0){
-      res.status(200).json({
-        "data": []
-      });
-    }
-    else if(err) {
-      logger.error("failed logging in parent");
-      res.status(400);
-    }
-    else{
-      res.status(200).json({
-        "data": rows
-      })
-    }
-  })
-}
